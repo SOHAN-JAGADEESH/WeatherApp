9b9612b0fe31131caba9049f15f1cab156eb185d
SOHAN-JAGADEESH <sohanjagadeesh@gmail.com>
2023-11-19 23:36:05 +1100

    WebApp finished
    All functionalities are working.

---
+++ script.js
@@ -0,0 +1,53 @@
+const apiKey = "ceb1d5d8b18e148b6b0a74e2601b5923";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display ="block";
+
+    }
+    else{
+        var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°" ;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + '%';
+    document.querySelector(".Wind").innerHTML = data.wind.speed + ' km/h';
+
+    if(data.weather[0].main == "Clouds")
+    {
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear")
+    {
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain")
+    {
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle")
+    {
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else
+    {
+        weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display ="block";
+    document.querySelector(".error").style.display ="none";
+
+    }
+    
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
